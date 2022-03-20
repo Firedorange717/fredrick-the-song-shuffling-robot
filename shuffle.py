810da99bdbb860e519c2cd603139fe4bac27f38c
Firedorange717 <joshuapk1234@gmail.com>
2022-03-19 17:48:40 -0700

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -7,7 +7,7 @@ print("Bi-Polar or Uni-Polar Stepper motor demo!")
 # make stepper motor a variable to make code shorter to type!
 stepper_motor = crickit.stepper_motor
 # increase to slow down, decrease to speed up!
-INTERSTEP_DELAY = 0.001
+INTERSTEP_DELAY = 0.0001
 
 while True:
     print("Double step")
