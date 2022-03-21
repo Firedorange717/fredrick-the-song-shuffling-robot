3b3760135caafa909c069285f33277085367d70d
Firedorange717 <joshuapk1234@gmail.com>
2022-03-20 21:20:16 -0700

    Create servoDemo.py

---
+++ servoDemo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import time
+from adafruit_crickit import crickit
+
+print("1 Servo demo!")
+angle = 0;
+
+while True:
+	print("Moving servo #1")
+	crickit.servo_1.angle = angle
+	time.sleep(10)
+	angle += 1
+	
+	if angle == 180:
+		angle = 0
+		time.sleep(10)
