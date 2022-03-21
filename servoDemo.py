e1ada54b645c854b46bc42743e26d49e431e51d8
Firedorange717 <joshuapk1234@gmail.com>
2022-03-20 21:21:06 -0700

    Update servoDemo.py

--- servoDemo.py
+++ servoDemo.py
@@ -9,9 +9,9 @@ angle = 0;
 while True:
 	print("Moving servo #1")
 	crickit.servo_1.angle = angle
-	time.sleep(10)
+	time.sleep(1)
 	angle += 1
 	
 	if angle == 180:
 		angle = 0
-		time.sleep(10)
+		time.sleep(1)
