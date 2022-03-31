534ed14bffb34108b414a8dddce7e96a7dc2695d
Firedorange717 <joshuapk1234@gmail.com>
2022-03-30 18:27:53 -0700

    Update servoDemo.py

--- servoDemo.py
+++ servoDemo.py
@@ -6,14 +6,16 @@ from adafruit_crickit import crickit
 angle = 0;
 
 while True:
-	crickit.servo_1.angle = angle
-	crickit.servo_2.angle = angle
-	angle += 1
-	print(angle)
-	
-	if angle == 180:
-		angle = 0
+		crickit.servo_1.angle = 180
+		crickit.servo_2.angle = 180
+		crickit.servo_3.angle = 180
+		time.sleep(3)
 		crickit.servo_1.angle = 90
 		crickit.servo_2.angle = 90
+		crickit.servo_3.angle = 90
+		time.sleep(3)
+		crickit.servo_1.angle = 0
+		crickit.servo_2.angle = 0
+		crickit.servo_3.angle = 0
 		time.sleep(3)
 		
