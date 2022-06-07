2005546988b8238d6be4780a16276e3cc0124eb0
Firedorange717 <joshuapk1234@gmail.com>
2022-06-06 23:35:42 -0700

    no message

--- servoDemo.py
+++ servoDemo.py
@@ -7,12 +7,12 @@ print("Bi-Polar or Uni-Polar Stepper motor demo!")
 # make stepper motor a variable to make code shorter to type!
 stepper_motor = crickit.stepper_motor
 # increase to slow down, decrease to speed up!
-INTERSTEP_DELAY = 0.001
+INTERSTEP_DELAY = 0.01
 
 while True:
     for i in range(400):
-        stepper_motor.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+        stepper_motor.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)
         time.sleep(INTERSTEP_DELAY)
     for i in range(400):
-        stepper_motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        stepper_motor.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
         time.sleep(INTERSTEP_DELAY)
